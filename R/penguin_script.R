1128d8c25bf611d95bd5ca1b5c308c824b2f7797
LesegoMalekana <153714247+LesegoMalekana@users.noreply.github.com>
2024-11-08 11:55:52 +0100

    Update penguin_script.R

--- R/penguin_script.R
+++ R/penguin_script.R
@@ -12,3 +12,7 @@ ggplot(palmerpenguins::penguins, aes(x = species, y = bill_length_mm)) +
 ggplot(palmerpenguins::penguins, aes(x = bill_depth_mm, y = bill_length_mm)) +
   geom_point(aes(colour = palmerpenguins::penguins$species)) +
   labs(x = "Bill Depth (mm)", y = "Bill Length (mm)")
+
+
+
+yah yah
